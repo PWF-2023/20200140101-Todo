54769aa8dc5c4f12070c8383a55a45c25b76778e
101-askar <askar.kadir.ft20@mail.umy.ac.id>
2023-04-01 22:03:35 +0700

    âmigration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,21 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
     // return view todo.index
-    public function index(){
+    public function index()
+    {
+        $todos = Todo::where('user_id', auth()->id())->get();
+        // collection of todos
+        dd($todos);
         return view('todo.index');
     }
 
     // return view todo.create
-    public function create(){
+    public function create()
+    {
         return view('todo.create');
     }
 
     // return view todo.edit
-    public function edit(){
+    public function edit()
+    {
         return view('todo.edit');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Todo;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        user::create(
+            [
+                'name'              => fake()->name(),
+                'email'             => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                // password
+                'remember_token'    => Str::random(10),
+                'is_admin'          => true,
+            ]
+        );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        user::create(
+            [
+                'name'              => 'admin',
+                'email'             => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                // password
+                'remember_token'    => Str::random(10),
+                'is_admin'          => true,
+            ]
+        );
+
+        user::create(
+            [
+                'name'              => 'Askar Kadir',
+                'email'             => 'askar@admin.com',
+                'email_verified_at' => now(),
+                'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+                // password
+                'remember_token'    => Str::random(10),
+                'is_admin'          => false,
+            ]
+        );
+
+
+
+        user::factory(100)->create();
+        Todo::factory(500)->create();
     }
 }
